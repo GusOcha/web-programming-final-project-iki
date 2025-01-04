66fd1abb1e8ee52acdde78e4d2ea2b3739926fec
Gus Ocha <gusocha1405@gmail.com>
2025-01-04 18:45:58 +0800

    Second Commit - API Token and User Middleware

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,35 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+// Login
+$router->get('/login', ['uses' => 'LoginController@index']);
 
-// Units Table CRUD
-$router->get('/units/read', 'UnitsController@index');
-$router->get('/units/read/{id}', 'UnitsController@show');
-$router->post('/units/create', 'UnitsController@store');
-$router->put('/units/update/{id}', 'UnitsController@update');
-$router->delete('/units/delete/{id}', 'UnitsController@destroy');
+$router->group(['prefix' => 'api', 'middleware'=>'auth'], function() use ($router){
+    // Units Table CRUD
+    $router->get('/units/read', ['uses' => 'UnitsController@index']);
+    $router->get('/units/read/{id}', ['uses' => 'UnitsController@show']);
+    $router->post('/units/create', ['uses' => 'UnitsController@store']);
+    $router->put('/units/update/{id}', ['uses' => 'UnitsController@update']);
+    $router->delete('/units/delete/{id}', ['uses' => 'UnitsController@destroy']);
 
-// Rooms table CRUD
-$router->get('/rooms/read', 'RoomsController@index');
-$router->get('/rooms/read/{id}', 'RoomsController@show');
-$router->post('/rooms/create', 'RoomsController@store');
-$router->put('/rooms/update/{id}', 'RoomsController@update');
-$router->delete('/rooms/delete/{id}', 'RoomsController@destroy');
+    // Rooms table CRUD
+    $router->get('/rooms/read', ['uses' => 'RoomsController@index']);
+    $router->get('/rooms/read/{id}', ['uses' => 'RoomsController@show']);
+    $router->post('/rooms/create', ['uses' => 'RoomsController@store']);
+    $router->put('/rooms/update/{id}', ['uses' => 'RoomsController@update']);
+    $router->delete('/rooms/delete/{id}', ['uses' => 'RoomsController@destroy']);
 
-// Users table CRUD
-$router->get('/users/read', 'UsersController@index');
-$router->get('/users/read/{id}', 'UsersController@show');
-$router->post('/users/create', 'UsersController@store');
-$router->put('/users/update/{id}', 'UsersController@update');
-$router->delete('/users/delete/{id}', 'UsersController@destroy');
+    // Users table CRUD
+    $router->get('/users/read', ['uses' => 'UsersController@index']);
+    $router->get('/users/read/{id}', ['uses' => 'UsersController@show']);
+    $router->post('/users/create', ['uses' => 'UsersController@store']);
+    $router->put('/users/update/{id}', ['uses' => 'UsersController@update']);
+    $router->delete('/users/delete/{id}', ['uses' => 'UsersController@destroy']);
 
-// Bookings table CRUD
-$router->get('/bookings/read', 'BookingsController@index');
-$router->get('/bookings/read/{id}', 'BookingsController@show');
-$router->post('/bookings/create', 'BookingsController@store');
-$router->put('/bookings/update/{id}', 'BookingsController@update');
-$router->delete('/bookings/delete/{id}', 'BookingsController@destroy');
+    // Bookings table CRUD
+    $router->get('/bookings/read', ['uses' => 'BookingsController@index']);
+    $router->get('/bookings/read/{id}', ['uses' => 'BookingsController@show']);
+    $router->post('/bookings/create', ['uses' => 'BookingsController@store']);
+    $router->put('/bookings/update/{id}', ['uses' => 'BookingsController@update']);
+    $router->delete('/bookings/delete/{id}', ['uses' => 'BookingsController@destroy']);
+});
