768b4e1a89e42e6c76f68f8d7a8684de999e8fcf
Gus Ocha <gusocha1405@gmail.com>
2025-01-05 18:19:29 +0800

    Fourth Commit - Login and Register with Hash Password and Validator

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,48 +4,89 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
     public function register(Request $request){
-        $data = [
-            'unit_id' => $request->input('unit_id'),
-            'user_name' => $request->input('user_name'),
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-            'role' => 'user',
-            'api_token' => 'user_token'
-        ];
+        $validator = Validator::make($request->all(), [
+            'unit_id' => 'required',
+            'user_name' => 'required|unique:users',
+            'email' => 'required|unique:users',
+            'password' => [
+                'required',
+                'min:8',
+                'regex:/[a-z]/',
+                'regex:/[A-Z]/',
+                'regex:/[0-9]/'
+            ]
+        ]);
 
-        User::create($data);
-
-        return response()->json($data);
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Semua data wajib diisi!',
+                'data' => $validator->errors()
+            ], 401);
+        }
+        else{
+            $data = [
+                'unit_id' => $request->input('unit_id'),
+                'user_name' => $request->input('user_name'),
+                'email' => $request->input('email'),
+                'password' => $request->input('password'),
+                'role' => 'user',
+                'api_token' => 'user_token'
+            ];
+    
+            $data['password'] = Hash::make($data['password']);
+    
+            User::create($data);
+    
+            return response()->json([
+                    'message' => 'Register berhasil!',
+                    'data' => $data
+            ]);
+        }
     }
 
     public function login(Request $request){ 
-        $email = $request->input('email');
-        $password = $request->input('password');
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
 
-        $user = User::where('email', $email)->first();
-
-        if($user->password === $password){
-            $token = Str::random(40);
-
-            $user->update([
-                'api_token' => $token
-            ]);
-
+        if($validator->fails()){
             return response()->json([
-                'message' => 'Login berhasil!',
-                'token' => $token,
-                'data' => $user
-            ]);
+                'message' => 'Semua data wajib diisi!',
+                'data' => $validator->errors()
+            ], 401);
         }
         else{
-            return response()->json([
-                'message' => 'Login gagal!',
-                'data' => ''
-            ]); 
+            $email = $request->input('email');
+            $password = $request->input('password');
+    
+            $user = User::where('email', $email)->first();
+    
+            if($user && Hash::check($password, $user->password)){
+                $token = Str::random(40);
+    
+                $user->update([
+                    'api_token' => $token
+                ]);
+    
+                return response()->json([
+                    'message' => 'Login berhasil!',
+                    'token' => $token,
+                    'data' => $user
+                ]);
+            }
+            else{
+                return response()->json([
+                    'message' => 'Login gagal!',
+                    'data' => ''
+                ]); 
+            }
         }
     }
 }
